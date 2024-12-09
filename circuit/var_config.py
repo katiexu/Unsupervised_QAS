c42b184ec252aad6575f96403047a620467e9090
katiexu <xuke0531@hotmail.com>
2024-12-09 17:35:23 +0800

    Delete circuit directory

--- circuit/var_config.py
+++
@@ -1,7 +0,0 @@
-### Configure gloabal variables ###
-
-max_depth = 5
-num_qubits = 4
-num_gates = 10 ### TODO: Can it be not defined in advance? (different-size Tensor), maybe padding with I gate
-num_circuits = 100000
-allowed_gates = ['PauliX', 'PauliY', 'PauliZ', 'Hadamard', 'RX', 'RY', 'RZ', 'CNOT', 'CZ', 'U3', 'SWAP']
