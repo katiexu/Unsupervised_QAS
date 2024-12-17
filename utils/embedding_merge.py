a51aa1ad1123aa18135029536a0d48668b0f03cf
katiexu <xuke0531@hotmail.com>
2024-12-18 04:04:05 +0800

    Delete utils/embedding_merge.py

--- utils/embedding_merge.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-if __name__ == '__main__':
-    embedding = torch.load('pretrained\\dim-16\\maxcut-model-circuits_4_qubits_full_embedding.pt')
-    feature_embedding = torch.load('pretrained\\dim-16\\maxcut-model-circuits_4_qubits_1.pt')
-    for ind in range(len(embedding)):
-        embedding[ind]['feature'] = feature_embedding[ind]['feature']
-    torch.save(embedding, 'pretrained\\dim-16\\maxcut-model-circuits_4_qubits_full_embedding_1.pt')
